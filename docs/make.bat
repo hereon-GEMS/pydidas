28d741ae13309cbefe2e1645eb82f6abad47baf1
Malte Storm <malte.storm@hereon.de>
2022-01-07 12:12:43 +0100

    Updated make files for github-pages documentation.

--- docs/make.bat
+++ docs/make.bat
@@ -9,8 +9,9 @@ if "%SPHINXBUILD%" == "" (
 )
 set SOURCEDIR=source
 set BUILDDIR=build
-
+set GH_PAGES_SOURCES=source pydidas make.bat
 if "%1" == "" goto help
+if "%1" == "gh-pages" goto gh-pages
 
 %SPHINXBUILD% >NUL 2>NUL
 if errorlevel 9009 (
@@ -25,8 +26,32 @@ if errorlevel 9009 (
 	exit /b 1
 )
 
+
+    git checkout gh-pages
+    rm -rf build _sources _static
+    git checkout master $(GH_PAGES_SOURCES)
+    git reset HEAD
+    make html
+    mv -fv build/html/* ./
+    rm -rf $(GH_PAGES_SOURCES) build
+    git add -A
+    git ci -m "Generated gh-pages for `git log master -1 --pretty=short --abbrev-commit`" && git push origin gh-pages ; git checkout master
+
+
 %SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
 goto end
+
+:gh-pages
+git checkout gh-pages
+del /r /f  build _sources _static
+git checkout master %GH_PAGES_SOURCES%
+git reset HEAD
+./make.bat html
+move /Y build/html/* ./
+del /r /f %GH_PAGES_SOURCES% build
+git add -A
+git ci -m "Generated gh-pages for `git log master -1 --pretty=short --abbrev-commit`" && git push origin gh-pages ; git checkout master
+
 
 :help
 %SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%

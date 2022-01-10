f05271ee2660056d52b1b12dec0c34f8dc1ac034
Malte Storm <malte.storm@hereon.de>
2022-01-10 08:47:00 +0100

    Work on sphinx script for automatic github pages.

--- docs/make.bat
+++ docs/make.bat
@@ -27,17 +27,6 @@ if errorlevel 9009 (
 )
 
 
-    git checkout gh-pages
-    rm -rf build _sources _static
-    git checkout master $(GH_PAGES_SOURCES)
-    git reset HEAD
-    make html
-    mv -fv build/html/* ./
-    rm -rf $(GH_PAGES_SOURCES) build
-    git add -A
-    git ci -m "Generated gh-pages for `git log master -1 --pretty=short --abbrev-commit`" && git push origin gh-pages ; git checkout master
-
-
 %SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
 goto end
 

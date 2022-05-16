6c1d346e27f7c9ef281cf3bb4165f6d93f3b3282
Malte Storm <malte.storm@hereon.de>
2022-05-16 08:13:28 +0200

    incremented version to 0.1.8

--- pydidas/docs/source/conf.py
+++ pydidas/docs/source/conf.py
@@ -25,7 +25,7 @@ author = "Malte Storm"
 
 # The full version, including alpha/beta/rc tags
 release = "alpha"
-version = "0.1.7"
+version = "0.1.8"
 
 # -- General configuration ---------------------------------------------------
 

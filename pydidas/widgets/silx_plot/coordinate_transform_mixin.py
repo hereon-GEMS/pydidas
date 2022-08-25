b576067c5051ecfb8b1ba79400dbcb60021018f0
Malte Storm <malte.storm@hereon.de>
2022-08-25 16:21:35 +0200

    Moved coordinate transformations to mix-in class to allow Integration in other widgets as well.

--- pydidas/widgets/silx_plot/coordinate_transform_mixin.py
+++ pydidas/widgets/silx_plot/coordinate_transform_mixin.py
@@ -14,7 +14,8 @@
 # along with Pydidas. If not, see <http://www.gnu.org/licenses/>.
 
 """
-Module with silx actions for PlotWindows.
+Module with CoordinateTransformMixin to add coordinate transform functionality to other
+silx plot widgets.
 """
 
 __author__ = "Malte Storm"
@@ -22,57 +23,27 @@ __copyright__ = "Copyright 2021-2022, Malte Storm, Helmholtz-Zentrum Hereon"
 __license__ = "GPL-3.0"
 __maintainer__ = "Malte Storm"
 __status__ = "Development"
-__all__ = ["PydidasPlot2D"]
+__all__ = ["CoordinateTransformMixin"]
 
 import numpy as np
-from qtpy import QtCore
-from silx.gui.plot import Plot2D
 
 from ...experiment import SetupExperiment
-from .silx_actions import ChangeCanvasToData, ExpandCanvas, CropHistogramOutliers
-from .coordinate_transform_button import CoordinateTransformButton
-from .coordinate_transform_mixin import CoordinateTransformMixin
+
 
 EXP_SETUP = SetupExperiment()
 
 
-class PydidasPlot2D(CoordinateTransformMixin, Plot2D):
+class CoordinateTransformMixin:
     """
-    A customized silx.gui.plot.Plot2D with an additional features to limit the figure
-    canvas to the data limits.
+    Mix-in class to add coordinate transform functionality to silx plot widgets.
+
+    The main class must be a silx.gui.plot.PlotWindow or subclass to have access to
+    its backend functionality.
     """
-
-    def __init__(self, parent=None, backend=None):
-        Plot2D.__init__(self, parent, backend)
-        CoordinateTransformMixin.__init__(self)
-
-        self.changeCanvasToDataAction = self.group.addAction(
-            ChangeCanvasToData(self, parent=self)
-        )
-        self.changeCanvasToDataAction.setVisible(True)
-        self.addAction(self.changeCanvasToDataAction)
-        self._toolbar.insertAction(self.colormapAction, self.changeCanvasToDataAction)
-
-        self.expandCanvasAction = self.group.addAction(ExpandCanvas(self, parent=self))
-        self.expandCanvasAction.setVisible(True)
-        self.addAction(self.expandCanvasAction)
-        self._toolbar.insertAction(self.colormapAction, self.expandCanvasAction)
-
-        self.cropHistOutliersAction = self.group.addAction(
-            CropHistogramOutliers(self, parent=self)
-        )
-        self.cropHistOutliersAction.setVisible(True)
-        self.addAction(self.cropHistOutliersAction)
-        self._toolbar.insertAction(
-            self.keepDataAspectRatioAction, self.cropHistOutliersAction
-        )
-
-        self.cs_transform = CoordinateTransformButton(parent=self, plot=self)
-        self._toolbar.addWidget(self.cs_transform)
-
-        self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+    def __init__(self, **kwargs):
         self._beam_center = (0, 0, 0.1)
         self._pixelsize = (100e-6, 100e-6)
+        assert hasattr(self, "_backend")
 
     def pixelToData_cartesian(self, x, y, axis="left", check=False):
         """

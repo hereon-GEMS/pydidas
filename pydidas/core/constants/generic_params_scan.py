3c9e9ecfa672b5ff45a73c7476d732dc1d8472d3
Malte Storm <malte.storm@hereon.de>
2023-02-16 09:51:50 +0100

    Extended tooltip for scan image multiplicity for more clarity.

--- pydidas/core/constants/generic_params_scan.py
+++ pydidas/core/constants/generic_params_scan.py
@@ -128,8 +128,11 @@ GENERIC_PARAMS_SCAN = (
             "unit": "",
             "allow_None": False,
             "tooltip": (
-                "The number of images acquired at each scan point. The default of '1' "
-                "corresponds to one image per scan point."
+                "The number of images acquired at *each* scan point. The default of "
+                "'1' corresponds to one image per scan point. Please note that the "
+                "value for the multiplicity will be multiplied with the number of scan "
+                "points. If this setting is used for 'averaging' images, please reduce "
+                "the number of scan points correspondingly."
             ),
         },
     }

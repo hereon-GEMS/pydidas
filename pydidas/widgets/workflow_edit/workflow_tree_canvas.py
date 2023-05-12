d61e767fd5f7bbc485f05776ee2c405b0317c775
Malte Storm <malte.storm@hereon.de>
2023-05-12 09:15:29 +0200

    Removed redundant import from the workflow_tree_canvas module.

--- pydidas/widgets/workflow_edit/workflow_tree_canvas.py
+++ pydidas/widgets/workflow_edit/workflow_tree_canvas.py
@@ -1,9 +1,11 @@
 # This file is part of pydidas.
 #
+# Copyright 2021-, Helmholtz-Zentrum Hereon
+# SPDX-License-Identifier: GPL-3.0-only
+#
 # pydidas is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
 #
 # Pydidas is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -13,22 +15,24 @@
 # You should have received a copy of the GNU General Public License
 # along with Pydidas. If not, see <http://www.gnu.org/licenses/>.
 
+
 """
 Module with the WorkflowTreeCanvas which is used to arrange the widgets for
 editing the WorkflowTree.
 """
 
 __author__ = "Malte Storm"
-__copyright__ = "Copyright 2021-2022, Malte Storm, Helmholtz-Zentrum Hereon"
-__license__ = "GPL-3.0"
+__copyright__ = "Copyright 2023, Helmholtz-Zentrum Hereon"
+__license__ = "GPL-3.0-only"
 __maintainer__ = "Malte Storm"
-__status__ = "Development"
+__status__ = "Production"
 __all__ = ["WorkflowTreeCanvas"]
 
-from qtpy import QtWidgets, QtGui, QtCore
+
+from qtpy import QtCore, QtGui, QtWidgets
 
 from ...core import constants
-from ..factory import create_label, CreateWidgetsMixIn
+from ..factory import CreateWidgetsMixIn
 
 
 class WorkflowTreeCanvas(QtWidgets.QFrame, CreateWidgetsMixIn):

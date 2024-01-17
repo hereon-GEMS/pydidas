047571708bf7fe39703b760f2a9edd2f7a4e8f10
Malte Storm <malte.storm@hereon.de>
2024-01-17 14:41:26 +0100

    Minor cosmetic improvements in UI.

--- pydidas/widgets/windows/manually_set_beamcenter_window.py
+++ pydidas/widgets/windows/manually_set_beamcenter_window.py
@@ -1,6 +1,6 @@
 # This file is part of pydidas.
 #
-# Copyright 2023, Helmholtz-Zentrum Hereon
+# Copyright 2024, Helmholtz-Zentrum Hereon
 # SPDX-License-Identifier: GPL-3.0-only
 #
 # pydidas is free software: you can redistribute it and/or modify
@@ -21,7 +21,7 @@ in an image to define the beamcenter.
 """
 
 __author__ = "Malte Storm"
-__copyright__ = "Copyright 2023, Helmholtz-Zentrum Hereon"
+__copyright__ = "Copyright 2024, Helmholtz-Zentrum Hereon"
 __license__ = "GPL-3.0-only"
 __maintainer__ = "Malte Storm"
 __status__ = "Production"
@@ -226,14 +226,11 @@ class ManuallySetBeamcenterWindow(PydidasWindow):
         """
         _path = Path(mask_filename)
         if detector_name in PYFAI_DETECTOR_NAMES and not _path.is_file():
-            print("setting mask from detector")
             self._bc_controller.set_new_detector_with_mask(detector_name)
         elif _path.is_file():
-            print("new mask file selection:", _path)
             self._bc_controller.set_mask_file(_path)
         else:
             self._bc_controller.set_mask_file(None)
-            print("No mask")
 
     @QtCore.Slot()
     def _confirm_points(self):

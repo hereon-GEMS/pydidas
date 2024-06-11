c5faec32feb7dd903e793ed3ba77b8eb7d2e48b7
Gudrun Lotze <gudrun.lotze@gmail.com>
2024-06-11 12:28:29 +0200

    Included the header

---
+++ tests/_plugin_tests/proc_plugins/test_stress-strain.py
@@ -0,0 +1,28 @@
+# This file is part of pydidas.
+#
+# Copyright 2023 - 2024, Helmholtz-Zentrum Hereon
+# SPDX-License-Identifier: GPL-3.0-only
+#
+# pydidas is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# Pydidas is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pydidas. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+The dataset module includes the Dataset subclasses of numpy.ndarray with additional
+embedded metadata.
+"""
+
+__author__ = "Gudrun Lotze"
+__copyright__ = "Copyright 2024, Helmholtz-Zentrum Hereon"
+__license__ = "GPL-3.0-only"
+__maintainer__ = "Gudrun Lotze"
+__status__ = "Development"
+

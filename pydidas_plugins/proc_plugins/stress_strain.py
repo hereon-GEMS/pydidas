6cf9231ed3dae66109f14f8ad14db29d23f019d0
Gudrun Lotze <gudrun.lotze@gmail.com>
2024-06-28 17:06:13 +0200

    Working on d_spacing_combined variable and its metadata

--- pydidas_plugins/proc_plugins/stress_strain.py
+++ pydidas_plugins/proc_plugins/stress_strain.py
@@ -493,6 +493,7 @@ def combine_sort_d_spacing_pos_neg(d_spacing_pos, d_spacing_neg):
     d_spacing_combi_arr = np.vstack((d_spacing_neg_sorted, d_spacing_pos_sorted))
        
     #TODO: Is the data_label how we want them to be?
+    #TODO: Is the axis_label for idx 0 correct?
     d_spacing_combined = Dataset(d_spacing_combi_arr , 
                                  axis_ranges={0: np.arange(2), 1:  s2c_axis_pos_sorted}, 
                                  axis_labels={0: '0: d-, 1: d+', 1: 'sin^2(chi)'}, #TODO: previous: sin2chi

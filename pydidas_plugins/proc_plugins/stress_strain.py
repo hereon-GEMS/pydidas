c19f15dd5391820d5dba6a4ad191ba49f83fe3ff
Gudrun Lotze <gudrun.lotze@gmail.com>
2024-07-04 15:25:51 +0200

    Removed setting of data_unit as it is implemented

--- pydidas_plugins/proc_plugins/stress_strain.py
+++ pydidas_plugins/proc_plugins/stress_strain.py
@@ -488,7 +488,7 @@ def combine_sort_d_spacing_pos_neg(d_spacing_pos, d_spacing_neg):
        
     #TODO: Is the data_label how we want them to be?
     #TODO: Is the axis_label for idx 0 correct and a good axis label?
-    d_spacing_combined = Dataset(d_spacing_combi_arr , 
+    d_spacing_combined = Dataset(d_spacing_combi_arr, 
                                  axis_ranges={0: np.arange(2), 1:  s2c_axis_pos_sorted}, 
                                  axis_labels={0: '0: d-, 1: d+', 1: 'sin^2(chi)'},
                                  data_label=f'0: {d_spacing_neg.data_label}, 1: {d_spacing_pos.data_label}',
@@ -530,10 +530,10 @@ def pre_regression_calculation(d_spacing_combined):
         #d_spacing_avg= np.mean(d_spacing_combined, axis=0)
         #TODO: Now with Dataset.mean axis_ranges: 7.498799e-33, before: 7.49879891e-33. Precision??
         d_spacing_avg= d_spacing_combined.mean(axis=0)
-        d_spacing_avg.data_unit=d_spacing_combined.data_unit
-        #print('d_spacing_combined.axis_ranges[1]', d_spacing_combined.axis_ranges[1])
-        #d_spacing_avg.axis_ranges={0: d_spacing_combined.axis_ranges[1]}
-    
+        print(40*"\N{cactus}")
+        print('d_spacing_avg', d_spacing_avg)     
+      
+        print(40*"\N{cactus}")
             
         
         #d-, d+

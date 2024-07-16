b5185765371cf15691ee4f0f9d12ee0bb9c75d82
Malte Storm <malte.storm@hereon.de>
2024-07-16 11:00:38 +0200

    Updated the new documentation for generic Parameters with an introduction.

--- pydidas/core/generic_params/generic_params.py
+++ pydidas/core/generic_params/generic_params.py
@@ -20,7 +20,7 @@ The generic_params module holds the dictionary with information required for cre
 generic Parameters.
 """
 
-__author__ = "Malte Storm"
+__author__ = "Malte Storm, Nonni Heere"
 __copyright__ = "Copyright 2023 - 2024, Helmholtz-Zentrum Hereon"
 __license__ = "GPL-3.0-only"
 __maintainer__ = "Malte Storm"
@@ -51,37 +51,74 @@ for key, value in _metadata_by_module.items():
         GENERIC_PARAMS_METADATA[key2] = value2
 
 
-def create_generic_params_rst_docu():
+__DOC_FILE_HEADER = """
+.. |parameters| replace:: :py:class:`Parameters <pydidas.core.Parameter>`
+.. |param_collection| replace:: :py:class:`ParameterCollection <pydidas.core.ParameterCollection>`
+
+.. _generic_params:
+
+Generic Parameters
+==================
+
+This page gives a full list of all available generic |parameters| as a reference.
+
+Introduction
+------------
+
+For accessing generic |parameters|, use 
+:py:func:`~pydidas.core.get_generic_parameter` to access individual |parameters| 
+and :py:func:`~pydidas.core.get_generic_param_collection` to create a new
+|param_collection| from generic |parameters|.
+
+Examples are given below:
+
+.. code-block:: python
+
+    >>> from pydidas.core import get_generic_parameter
+    >>> from pydidas.core import get_generic_param_collection
+    >>> filename_param = get_generic_parameter('filename')
+    >>> filename_param
+    Parameter <filename: . (type: Path, default: .)>
+    >>> generic_params = get_generic_param_collection('filename', 'n_files')
+    >>> generic_params
+    {'filename': Parameter <filename: . (type: Path, default: .)>,
+    'n_files': Parameter <n_files: 0 (type: Integral, default: 0)>}
+
+List of generic Parameters
+--------------------------
+
+Generic |parameters| are sorted by their *use case*. The following sections are 
+available: 
+
+.. contents::
+   :local:
+
+
+"""
+
+def create_generic_params_rst_docs(filename: Path):
     """
     Creates a reStructuredText documentation file for the generic parameters.
 
-    This function generates a reStructuredText file that documents the generic parameters
-    of the codebase. It writes the documentation to the file located at `./dev_guide/generic_params.rst`.
+    This function generates a reStructuredText file that documents the generic
+    parameters of the codebase. It writes the documentation to the file located at
+    `./dev_guide/generic_params.rst`.
 
-    Parameters:
-        None
-
-    Returns:
-        None
+    Parameters
+    ----------
+    filename : Path
+        The path of the file to which the documentation will be written.
     """
-    doc_path = "./dev_guide/generic_params.rst"
-
-    with open(doc_path, "w", encoding="utf-8") as f:
-        f.write(".. _generic_params:\n\n")
-        f.write("Generic Parameters\n")
-        f.write("==================\n\n")
-        f.write("Params sorted by use case:\n\n")
-        f.write(".. contents::\n")
-        f.write("   :local:\n")
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(__DOC_FILE_HEADER)
         for module_name, module_items in _metadata_by_module.items():
             _name = (
                 module_name.removeprefix("generic_params_")
                 .capitalize()
                 .replace("_", " ")
             )
-            f.write("\n" + _name + "\n")
-            f.write("-" * len(_name) + "\n\n")
             f.write(
+                f"\n{_name}\n{'~' * len(_name)}\n\n"
                 ".. list-table::\n"
                 "   :widths: 20 80\n"
                 "   :header-rows: 1\n"
@@ -90,13 +127,10 @@ def create_generic_params_rst_docu():
                 "     - Description\n"
             )
             for param_key, param in module_items.items():
-                f.write(f"   * - :py:data:`{param_key}`\n")
-                f.write("     - | ")
-                if isinstance(param["type"], str):
-                    pass
-                elif param["type"] is None:
+                f.write(f"   * - :py:data:`{param_key}`\n     - | ")
+                if param["type"] is None:
                     param["type"] = "None"
-                else:
+                elif not isinstance(param["type"], str):
                     param["type"] = param["type"].__name__
                 f.write(
                     ("\n       | ").join(

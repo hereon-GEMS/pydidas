dfb9eaee381045d1c577bf3f1a700782a94b63b9
Gudrun Lotze <gudrun.lotze@gmail.com>
2024-08-06 15:44:42 +0200

    First import

---
+++ tests/_plugin_tests/proc_plugins/tests_sin2chi_grouping.py
@@ -0,0 +1,31 @@
+# This file is part of pydidas.
+#
+# Copyright 2023 - 2024, Helmholtz-Zentrum Hereon
+# SPDX-License-Identifier: GPL-3.0-only
+#
+# pydidas is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# Pydidas is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pydidas. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Tests for the DspacingSin2chiGrouping class / plugin.
+"""
+
+__author__ = "Gudrun Lotze"
+__copyright__ = "Copyright 2024, Helmholtz-Zentrum Hereon"
+__license__ = "GPL-3.0-only"
+__maintainer__ = "Gudrun Lotze"
+__status__ = "Development"
+
+
+
+from pydidas.plugins import PluginCollection
+plugin =  PluginCollection.get_plugin_by_name('DspacingSin2chiGrouping')()

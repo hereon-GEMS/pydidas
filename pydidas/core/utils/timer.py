656d1aa7f3d473b20774920f1fa34fbe1280464e
Malte Storm <malte.storm@hereon.de>
2024-09-18 13:06:58 +0200

    Updated structure of TimerSaveRuntime context and added unittests for TimerSaveRuntime.

--- pydidas/core/utils/timer.py
+++ pydidas/core/utils/timer.py
@@ -1,6 +1,6 @@
 # This file is part of pydidas.
 #
-# Copyright 2023, Helmholtz-Zentrum Hereon
+# Copyright 2023 - 2024, Helmholtz-Zentrum Hereon
 # SPDX-License-Identifier: GPL-3.0-only
 #
 # pydidas is free software: you can redistribute it and/or modify
@@ -20,7 +20,7 @@ The Timer context manager allows to time and print code runtimes.
 """
 
 __author__ = "Malte Storm"
-__copyright__ = "Copyright 2023, Helmholtz-Zentrum Hereon"
+__copyright__ = "Copyright 2023 - 2024, Helmholtz-Zentrum Hereon"
 __license__ = "GPL-3.0-only"
 __maintainer__ = "Malte Storm"
 __status__ = "Production"
@@ -81,11 +81,27 @@ class TimerSaveRuntime:
         self._tstart = 0
         self._tend = 0
 
-    def __enter__(self):
+    def __enter__(self) -> object:
         """Start the context manager."""
         self._tstart = time.perf_counter()
-        return lambda: self._tend - self._tstart
+        return self
 
     def __exit__(self, type_, value, traceback):
         """Exit the context manager."""
         self._tend = time.perf_counter()
+
+    def __call__(self) -> float:
+        """Return the runtime."""
+        return self.runtime
+
+    @property
+    def runtime(self) -> float:
+        """
+        Return the runtime.
+
+        Returns
+        -------
+        float
+            The runtime of the code block.
+        """
+        return self._tend - self._tstart

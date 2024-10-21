d1dc5025db143492207ee3960cd1f877106f2b7a
Gudrun Lotze <gudrun.lotze@gmail.com>
2024-10-21 12:43:47 +0200

    Remove arr and updated to s2c

--- pydidas_plugins/proc_plugins/sin2chi_grouping.py
+++ pydidas_plugins/proc_plugins/sin2chi_grouping.py
@@ -665,7 +665,6 @@ class DspacingSin2chiGrouping(ProcPlugin):
             raise TypeError("Chi needs to be an np.ndarray.")
 
         s2c = np.sin(np.deg2rad(chi)) ** 2
-        #arr = s2c
 
         # Create the similarity matrix
         similarity_matrix = np.abs(s2c[:, np.newaxis] - s2c[np.newaxis, :]) <= tolerance

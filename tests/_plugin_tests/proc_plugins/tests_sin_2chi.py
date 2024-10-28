94bd96d05a7143e6612547056e0e977d466424d0
Gudrun Lotze <gudrun.lotze@gmail.com>
2024-10-28 16:13:23 +0100

    First test for execute

--- tests/_plugin_tests/proc_plugins/tests_sin_2chi.py
+++ tests/_plugin_tests/proc_plugins/tests_sin_2chi.py
@@ -39,6 +39,44 @@ from pydidas.core.constants import PROC_PLUGIN, PROC_PLUGIN_INTEGRATED
 
 from pydidas.core import Dataset, UserConfigError
 
-from pydidas_plugins.proc_plugins.sin2chi_grouping import PARAMETER_KEEP_RESULTS, LABELS_SIN2CHI, LABELS_SIN_2CHI
+from pydidas_plugins.proc_plugins.sin_2chi import (PARAMETER_KEEP_RESULTS, LABELS_SIN2CHI, LABELS_SIN_2CHI, LABELS_SIN2CHI,
+                                                            LABELS_DIM0, UNITS_NANOMETER, UNITS_ANGSTROM)
 
+
+@pytest.fixture
+def plugin_fixture():
+    return PluginCollection().get_plugin_by_name('DspacingSin_2chi')()
+
+
+@pytest.fixture()
+def base_dataset_factory():
+    def factory(unit):
+        
+        data_array=np.array([[1]*5, [3]*5, [2]*5])
+        
+        return Dataset(
+            data_array,
+            axis_labels={0: LABELS_DIM0, 1: LABELS_SIN2CHI},
+            axis_ranges={ 0: np.arange(data_array.shape[0]), 1: np.full((1, data_array.shape[1]),np.sin(np.arctan(2)))**2},
+            axis_units={0: '' , 1:''},
+            data_label= 'd_spacing', 
+            data_unit = unit,
+        )
+    return factory
+
+
+def test_execute_validation(plugin_fixture, base_dataset_factory):
+    dataset = base_dataset_factory(UNITS_NANOMETER)
+    result , _ = plugin_fixture.execute(dataset)
+    
+    assert result is not None
+    assert result.data_unit == UNITS_NANOMETER
+    assert result.data_label == 'Difference of d(+) - d(-)'
+    assert result.axis_labels[0] == '0: d-, 1: d+, 2: d(+)-d(-)'
+    assert result.axis_labels[1] == LABELS_SIN_2CHI
+    assert result.axis_units[0] == ''
+    assert result.axis_units[1] == ''
+    assert np.all(result.axis_ranges[0] == dataset.axis_ranges[0])
+    assert np.all(result.axis_ranges[1] == np.full((1, result.shape[1]), np.sin(2*np.arctan(2))))
+    assert np.all(result.data == np.array([[1]*5, [3]*5, [2]*5]))   
 

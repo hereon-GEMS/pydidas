e1b5d34fd0b2638f3f7bfe8c42c15a10b318c959
Malte Storm <malte.storm@hereon.de>
2025-11-03 15:20:53 +0100

    Added private FrameSliceHandler class to handle slicing in multi-dim datasets

---
+++ src/pydidas/core/utils/_frame_slice_handler.py
@@ -0,0 +1,121 @@
+# This file is part of pydidas.
+#
+# Copyright 2025, Helmholtz-Zentrum Hereon
+# SPDX-License-Identifier: GPL-3.0-only
+#
+# pydidas is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# Pydidas is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pydidas. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Module with the FrameSliceHandler class which allows to manage slicing data in 1 axis.
+"""
+
+__author__ = "Malte Storm"
+__copyright__ = "Copyright 2025, Helmholtz-Zentrum Hereon"
+__license__ = "GPL-3.0-only"
+__maintainer__ = "Malte Storm"
+__status__ = "Production"
+__all__ = ["FrameSliceHandler"]
+
+import warnings
+from numbers import Integral
+
+from pydidas.core import UserConfigError
+
+
+class FrameSliceHandler:
+    """
+    Store frame selection information and handle the logic for slicing.
+
+    This class supports only one axis being sliced at a time.
+    """
+
+    def __init__(
+        self,
+        axis: Integral | None = None,
+        frame: Integral = 0,
+        shape: tuple[Integral, ...] = (),
+    ):
+        # Set _frame and _axis first to have references in setters
+        self._frame = 0
+        self._axis = None
+        self.shape = shape  # Use setter to validate shape first
+        self.axis = axis
+        self.frame = frame
+
+    @property
+    def axis(self) -> Integral | None:
+        return self._axis
+
+    @axis.setter
+    def axis(self, value: Integral | None):
+        if value is not None and (value < 0 or value >= self.ndim):
+            raise UserConfigError(
+                f"Axis {value} is out of bounds for data with {self.ndim} dimensions."
+            )
+        self._axis = value
+        self._check_frame_index_in__bounds()
+
+    @property
+    def frame(self) -> int:
+        return int(self._frame)
+
+    @frame.setter
+    def frame(self, value: Integral):
+        if not isinstance(value, Integral):
+            raise UserConfigError("Frame index must be an integer.")
+        if self.axis is not None and (value < 0 or value >= self.shape[self.axis]):
+            raise UserConfigError(
+                f"Frame {value} is out of bounds for axis {self.axis} with size "
+                f"{self.shape[self.axis]}."
+            )
+        self._frame = value
+
+    @property
+    def shape(self) -> tuple[Integral, ...]:
+        return self._shape
+
+    @shape.setter
+    def shape(self, new_shape: tuple[Integral, ...]):
+        if not isinstance(new_shape, tuple):
+            raise UserConfigError("Shape must be a tuple of positive integers.")
+        if not all(isinstance(dim, Integral) and dim > 0 for dim in new_shape):
+            raise UserConfigError("All dimensions in shape must be positive integers.")
+        self._shape = new_shape
+        self._check_frame_index_in__bounds()
+
+    @property
+    def ndim(self) -> int:
+        return len(self._shape)
+
+    @property
+    def slice(self) -> tuple[slice | int, ...]:
+        if self.axis is None:
+            return slice(None)
+        else:
+            return tuple(
+                self.frame if ax == self.axis else slice(None)
+                for ax in range(self.axis + 1)
+            )
+
+    @property
+    def indices(self) -> tuple[None | tuple[None | Integral], ...]:
+        return None if self.axis is None else ((None,) * self.axis + (self.frame,))
+
+    def _check_frame_index_in__bounds(self):
+        if self._axis is not None and self._frame >= self.shape[self._axis]:
+            self._frame = 0
+            warnings.warn(
+                "Frame index reset to 0 due to axis change and the current "
+                "frame index being out of bounds for the new axis.",
+                UserWarning,
+            )
---
+++ tests/core/utils/_test_frame_slice_handler.py
@@ -0,0 +1,144 @@
+# This file is part of pydidas.
+#
+# Copyright 2025, Helmholtz-Zentrum Hereon
+# SPDX-License-Identifier: GPL-3.0-only
+#
+# pydidas is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# Pydidas is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pydidas. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Module with the FrameSliceHandler class which allows to manage slicing data in 1 axis.
+"""
+
+__author__ = "Malte Storm"
+__copyright__ = "Copyright 2025, Helmholtz-Zentrum Hereon"
+__license__ = "GPL-3.0-only"
+__maintainer__ = "Malte Storm"
+__status__ = "Production"
+
+
+import pytest
+
+from pydidas.core import UserConfigError
+from pydidas.core.utils._frame_slice_handler import FrameSliceHandler
+
+
+def test_init__w_defaults():
+    handler = FrameSliceHandler()
+    assert handler.axis is None
+    assert handler.frame == 0
+    assert handler.shape == ()
+
+
+@pytest.mark.parametrize(
+    "kwargs",
+    [
+        {"axis": 2},
+        {"frame": None},
+        {"shape": (2, 4), "axis": 2},
+        {"shape": (3, 4), "axis": 1, "frame": 42},
+    ],
+)
+def test_init__w_invalid_values(kwargs):
+    with pytest.raises(UserConfigError):
+        FrameSliceHandler(**kwargs)
+
+
+@pytest.mark.parametrize(
+    "kwargs",
+    [
+        {"shape": (3, 4, 5), "axis": 2},
+        {"shape": (3, 4, 5), "frame": 2, "axis": 1},
+        {"shape": (10, 20)},
+    ],
+)
+def test_init__w_valid_values(kwargs):
+    handler = FrameSliceHandler(**kwargs)
+    for _key, _value in kwargs.items():
+        assert getattr(handler, _key) == _value
+
+
+@pytest.mark.parametrize("axis", [1, None])
+def test_axis_setter__valid(axis):
+    handler = FrameSliceHandler(shape=(3, 4), frame=2)
+    handler.axis = axis
+    assert handler.axis == axis
+    assert handler.frame == 2
+
+
+@pytest.mark.parametrize("axis", [-1, 3])
+def test_axis_setter__out_of_bounds(axis):
+    handler = FrameSliceHandler(shape=(3, 4))
+    with pytest.raises(UserConfigError):
+        handler.axis = axis
+
+
+def test_axis_setter__w_frame_reset():
+    handler = FrameSliceHandler(shape=(3, 4), frame=3, axis=1)
+    handler.axis = 0
+    assert handler.axis == 0
+    assert handler.frame == 0
+
+
+def test_frame_setter__valid():
+    handler = FrameSliceHandler(shape=(3, 4), axis=1)
+    handler.frame = 2
+    assert handler.frame == 2
+
+
+@pytest.mark.parametrize("frame", [-1, 4, 3.3])
+def test_frame_setter__out_of_bounds(frame):
+    handler = FrameSliceHandler(shape=(3, 4), axis=1)
+    with pytest.raises(UserConfigError):
+        handler.frame = frame
+
+
+def test_shape_setter__valid():
+    handler = FrameSliceHandler()
+    handler.shape = (5, 4)
+    assert handler.shape == (5, 4)
+    assert handler.ndim == 2
+
+
+@pytest.mark.parametrize("shape", [[1, 21], {1, 2}, (5, 0), (3, -2), (4, 2.5)])
+def test_shape_setter__invalid(shape):
+    handler = FrameSliceHandler()
+    with pytest.raises(UserConfigError):
+        handler.shape = shape
+
+
+def test_slice_property__no_axis():
+    handler = FrameSliceHandler(shape=(3, 4))
+    assert handler.slice == slice(None)
+
+
+@pytest.mark.parametrize("axis", [0, 1, 2])
+def test_slice_property__with_axis(axis):
+    handler = FrameSliceHandler(shape=(3, 4, 5), axis=1, frame=2)
+    handler.axis = axis
+    assert handler.slice == (slice(None),) * axis + (2,)
+
+
+def test_indices_property__no_axis():
+    handler = FrameSliceHandler(shape=(3, 4))
+    assert handler.indices is None
+
+
+@pytest.mark.parametrize("axis", [0, 1, 2])
+def test_indices_property__with_axis(axis):
+    handler = FrameSliceHandler(shape=(3, 4, 5), axis=1, frame=2)
+    handler.axis = axis
+    assert handler.indices == (None,) * axis + (2,)
+
+
+if __name__ == "__main__":
+    pytest.main([])

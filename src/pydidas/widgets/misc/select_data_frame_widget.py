0ffcd6abe80a4d9103516027985844090b7a6fdb
Malte Storm <malte.storm@hereon.de>
2025-11-19 09:02:05 +0100

    Finished tests for SelectDataFrameWidget

--- src/pydidas/widgets/misc/select_data_frame_widget.py
+++ src/pydidas/widgets/misc/select_data_frame_widget.py
@@ -28,19 +28,18 @@ __status__ = "Production"
 __all__ = ["SelectDataFrameWidget"]
 
 
-from pathlib import Path
 from typing import Any
 
 from qtpy import QtCore
 
-from pydidas.core import UserConfigError, get_generic_param_collection
+from pydidas.core import get_generic_param_collection
 from pydidas.core.utils import (
     get_hdf5_metadata,
     get_hdf5_populated_dataset_keys,
     is_hdf5_filename,
 )
 from pydidas.core.utils._frame_slice_handler import FrameSliceHandler
-from pydidas.data_io import IoManager
+from pydidas.core.utils.hdf5_dataset_utils import get_generic_dataset
 from pydidas.widgets.data_viewer import DataAxisSelector
 from pydidas.widgets.file_dialog import PydidasFileDialog
 from pydidas.widgets.widget_with_parameter_collection import (
@@ -66,42 +65,37 @@ class SelectDataFrameWidget(WidgetWithParameterCollection):
             The number of dimensions of the data to be imported. The default is 2.
     """
 
-    sig_new_file_selection = QtCore.Signal(str, dict)
+    sig_new_selection = QtCore.Signal(str, dict)
     sig_file_valid = QtCore.Signal(bool)
     init_kwargs = WidgetWithParameterCollection.init_kwargs + [
         "import_reference",
         "ndim",
     ]
     default_params = get_generic_param_collection(
-        "filename",
-        "hdf5_key_str",
-        "slicing_axis",
+        "filename", "hdf5_key_str", "slicing_axis"
     )
 
     def __init__(self, **kwargs: Any):
         WidgetWithParameterCollection.__init__(self, **kwargs)
         self.set_default_params()
-        self.params["slicing_axis"].update_value_and_choices(None, [None])
+        self.set_param_value_and_choices("slicing_axis", None, [None])
         self.__import_dialog = PydidasFileDialog()
         self.__import_qref = kwargs.get("import_reference", None)
         self.__ndim = kwargs.get("ndim", 2)
-        self.__selection = FrameSliceHandler()
+        self._selection = FrameSliceHandler()
         self._create_widgets()
         self.connect_signals()
-        for _key in ["filename", "hdf5_key_str", "slicing_axis"]:
-            self.update_widget_value(_key, self.get_param_value(_key))
-        with QtCore.QSignalBlocker(self):
-            self._toggle_file_selection(self.get_param_value("filename").is_file())
+        self._toggle_file_selection(False, emit_signal=False)
 
     def _create_widgets(self):
         """Create the widgets for the data frame selection."""
         self.create_param_widget(
             "filename", linebreak=True, persistent_qsettings_ref=self.__import_qref
         )
-        self.create_param_widget("hdf5_key_str", visible=False)
-        self.create_param_widget("slicing_axis", visible=False)
+        self.create_param_widget("hdf5_key_str", width_text=0.4)
+        self.create_param_widget("slicing_axis", width_text=0.4)
         self.create_any_widget(
-            "ax_selector",
+            "index_selector",
             DataAxisSelector,
             0,
             multiline=True,
@@ -110,32 +104,19 @@ class SelectDataFrameWidget(WidgetWithParameterCollection):
 
     def connect_signals(self):
         """Connect the widget signals to the relevant slots."""
-        #        self._widgets["but_open"].clicked.connect(self.open_image_dialog)
-        self.param_widgets["filename"].sig_new_value.connect(self.process_new_filename)
-        self.param_widgets["hdf5_key_str"].sig_new_value.connect(
+        self.param_composite_widgets["filename"].sig_value_changed.connect(
+            self.process_new_filename
+        )
+        self.param_composite_widgets["hdf5_key_str"].sig_new_value.connect(
             self._selected_new_dset
         )
-        self.param_widgets["slicing_axis"].sig_new_value.connect(
+        self.param_composite_widgets["slicing_axis"].sig_new_value.connect(
             self._selected_new_slicing_axis
         )
-        self._widgets["ax_selector"].sig_new_slicing.connect(self._new_frame_index)
+        self._widgets["index_selector"].sig_new_slicing.connect(self._new_frame_index)
 
     @QtCore.Slot()
-    def open_image_dialog(self):
-        """
-        Open the image selected through the filename.
-        """
-        _fname = self.__import_dialog.get_existing_filename(
-            caption="Import image",
-            formats=IoManager.get_string_of_formats(),
-            qsettings_ref=self.__import_qref,
-        )
-        if _fname is not None:
-            self.set_param_value_and_widget("filename", _fname)
-            self.process_new_filename(_fname)
-
-    @QtCore.Slot(str)
-    def process_new_filename(self, filename: Path | str):
+    def process_new_filename(self):
         """
         Process the input of a new filename in the Parameter widget.
 
@@ -144,21 +125,20 @@ class SelectDataFrameWidget(WidgetWithParameterCollection):
         filename : Path | str
             The filename for the new input data.
         """
-        if not isinstance(filename, Path):
-            filename = Path(filename)
-        if not filename.is_file():
+        _fname = self.get_param_value("filename")
+        if not _fname.is_file():
             self._toggle_file_selection(False)
-            raise UserConfigError(
+            self.raise_UserConfigError(
                 "The selected filename is not a valid file. Please check the input "
                 "and correct the path."
             )
-        if is_hdf5_filename(filename):
-            self._process_new_hdf5_filename(filename)
+        if is_hdf5_filename(_fname):
+            self._process_new_hdf5_filename()
         else:
             self._selected_new_frame()
         self._toggle_file_selection(True)
 
-    def _toggle_file_selection(self, selected: bool):
+    def _toggle_file_selection(self, selected: bool, emit_signal: bool = True):
         """
         Modify widgets visibility and activation based on the file selection.
 
@@ -166,63 +146,38 @@ class SelectDataFrameWidget(WidgetWithParameterCollection):
         ----------
         selected : bool
             Flag to process.
+        emit_signal : bool, optional
+            Flag to emit the file valid signal. The default is True.
         """
         _is_hdf5 = is_hdf5_filename(self.get_param_value("filename"))
-        _show_selection = selected and _is_hdf5 and self.__selection.ndim > self.__ndim
+        _show_selection = selected and _is_hdf5 and self._selection.ndim > self.__ndim
         self.param_composite_widgets["hdf5_key_str"].setVisible(selected and _is_hdf5)
         self.param_composite_widgets["slicing_axis"].setVisible(_show_selection)
-        self._widgets["ax_selector"].setVisible(_show_selection)
-        self.sig_file_valid.emit(selected)
+        self._widgets["index_selector"].setVisible(_show_selection)
+        if emit_signal:
+            self.sig_file_valid.emit(selected)
 
-    def _process_new_hdf5_filename(self, filename: Path):
-        """
-        Process a new HDF5 filename: update dataset keys and axis selector.
-
-        Parameters
-        ----------
-        filename : Path
-            The HDF5 filename.
-        """
+    def _process_new_hdf5_filename(self):
+        """Process a new HDF5 filename: update dataset keys and axis selector."""
         _current_dset = self.get_param_value("hdf5_key_str", dtype=str)
         _dsets = get_hdf5_populated_dataset_keys(
-            filename, min_dim=self.__ndim, max_dim=self.__ndim + 1
+            self.get_param_value("filename"),
+            min_dim=self.__ndim,
+            max_dim=self.__ndim + 1,
         )
         if not _dsets:
             self._toggle_file_selection(False)
-            raise UserConfigError(
+            self.raise_UserConfigError(
                 "The selected HDF5 file does not contain any datasets with "
                 f"{self.__ndim} or {self.__ndim + 1} dimensions. Please select "
                 "a different file."
             )
         if _current_dset not in _dsets:
-            _current_dset = _dsets[0]
-        self.params["hdf5_key_str"].update_value_and_choices(_current_dset, _dsets)
-        self.param_widgets["hdf5_key_str"].update_choices(
-            _dsets, selection=_current_dset
+            _current_dset = get_generic_dataset(_dsets)
+        self.set_param_and_widget_value_and_choices(
+            "hdf5_key_str", _current_dset, _dsets, emit_signal=False
         )
         self._selected_new_dset(_current_dset)
-
-    @QtCore.Slot()
-    def _selected_new_frame(self):
-        """
-        Open a new file / frame based on the input Parameters.
-        """
-        _fname = self.get_param_value("filename", dtype=str)
-        _slice_ax = self.get_param_value("slicing_axis")
-        _options = {
-            "dataset": self.get_param_value("hdf5_key_str", dtype=str),
-            "indices": self.__selection.indices,
-        }
-        self.sig_new_file_selection.emit(_fname, _options)
-
-    def reset_selection(self):
-        """
-        Reset the file selection to default values.
-        """
-        self.set_param_value_and_widget("filename", "")
-        self.set_param_value_and_widget("hdf5_key_str", None)
-        with QtCore.QSignalBlocker(self):
-            self._toggle_file_selection(self.get_param_value("filename").is_file())
 
     @QtCore.Slot(str)
     def _selected_new_dset(self, dataset: str):
@@ -236,21 +191,20 @@ class SelectDataFrameWidget(WidgetWithParameterCollection):
         """
         _filename = self.get_param_value("filename", dtype=str)
         _meta = get_hdf5_metadata(f"{_filename}://{dataset}", ["shape", "ndim"])
-        self.__selection.shape = _meta["shape"]
-        if self.__selection.ndim == self.__ndim:
-            self.params["slicing_axis"].update_value_and_choices(None, [None])
-            self.param_widgets["slicing_axis"].update_choices([None], emit_signal=False)
-        else:  #  self.__selection.ndim == self.__ndim + 1
+        self._selection.shape = _meta["shape"]
+        if self._selection.ndim == self.__ndim:
+            self._selection.axis = None
+            self.set_param_and_widget_value_and_choices("slicing_axis", None, [None])
+        else:  #  self._selection.ndim == self.__ndim + 1
             _curr_ax = self.get_param_value("slicing_axis") or 0
-            _choices = list(range(self.__selection.ndim))
-            self.params["slicing_axis"].update_value_and_choices(_curr_ax, _choices)
-            self.param_widgets["slicing_axis"].update_choices(
-                _choices, selection=_curr_ax, emit_signal=False
+            _choices = list(range(self._selection.ndim))
+            self.set_param_and_widget_value_and_choices(
+                "slicing_axis", _curr_ax, _choices, emit_signal=False
             )
             self._selected_new_slicing_axis(_curr_ax, emit_signal=False)
-        self._widgets["ax_selector"].setVisible(self.__selection.ndim > self.__ndim)
-        self.param_composite_widgets["slicing_axis"].setVisible(
-            self.__selection.ndim > self.__ndim
+        self._widgets["index_selector"].setVisible(self._selection.ndim > self.__ndim)
+        self.toggle_param_widget_visibility(
+            "slicing_axis", self._selection.ndim > self.__ndim
         )
         self._selected_new_frame()
 
@@ -269,14 +223,34 @@ class SelectDataFrameWidget(WidgetWithParameterCollection):
         if axis_str == "None":
             return
         _ax = int(axis_str)
-        self.__selection.axis = _ax
-        with QtCore.QSignalBlocker(self._widgets["ax_selector"]):
-            self._widgets["ax_selector"].set_axis_metadata(
-                None, npoints=self.__selection.shape[_ax], ndim=1, dim_label="Frame:"
+        self._selection.axis = _ax
+        with QtCore.QSignalBlocker(self._widgets["index_selector"]):
+            self._widgets["index_selector"].set_axis_metadata(
+                None, npoints=self._selection.shape[_ax], ndim=1, dim_label="Frame:"
             )
-            self._widgets["ax_selector"].set_to_value(self.__selection.frame)
+            self._widgets["index_selector"].set_to_value(self._selection.frame)
         if emit_signal:
             self._selected_new_frame()
+
+    @QtCore.Slot()
+    def _selected_new_frame(self):
+        """
+        Open a new file / frame based on the input Parameters.
+        """
+        _fname = self.get_param_value("filename", dtype=str)
+        _options = {"indices": self._selection.indices}
+        if is_hdf5_filename(_fname):
+            _options["dataset"] = self.get_param_value("hdf5_key_str", dtype=str)
+        self.sig_new_selection.emit(_fname, _options)
+
+    # TODO : check if really not needed and whether to remove
+    # def reset_selection(self):
+    #     """
+    #     Reset the file selection to default values.
+    #     """
+    #     self.set_param_and_widget_value("filename", "")
+    #     self.set_param_and_widget_value("hdf5_key_str", None)
+    #     self._toggle_file_selection(False, emit_signal=False)
 
     @QtCore.Slot(int, str)
     def _new_frame_index(self, ax_index: int, frame_slice_str: str):
@@ -292,5 +266,5 @@ class SelectDataFrameWidget(WidgetWithParameterCollection):
         """
         del ax_index
         _index = int(frame_slice_str.split(":")[0])
-        self.__selection.frame = _index
+        self._selection.frame = _index
         self._selected_new_frame()
---
+++ tests/widgets/misc/test_select_data_frame_widget.py
@@ -0,0 +1,347 @@
+# This file is part of pydidas.
+#
+# Copyright 2025, Helmholtz-Zentrum Hereon
+# SPDX-License-Identifier: GPL-3.0-only
+#
+# pydidas is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# Pydidas is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pydidas. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Module with the SelectDataFrameWidget for selecting data frames from a dataset.
+"""
+
+__author__ = "Malte Storm"
+__copyright__ = "Copyright 2025, Helmholtz-Zentrum Hereon"
+__license__ = "GPL-3.0-only"
+__maintainer__ = "Malte Storm"
+__status__ = "Production"
+
+import warnings
+from pathlib import Path
+from unittest.mock import patch
+
+import h5py
+import numpy as np
+import pytest
+from qtpy import QtCore
+from skimage.io import imsave
+
+from pydidas.core import UserConfigError
+from pydidas.unittest_objects import SignalSpy
+from pydidas.widgets.misc.select_data_frame_widget import SelectDataFrameWidget
+from pydidas_qtcore import PydidasQApplication
+
+
+_VALID_FILENAMES = ["hdf5_file.h5", "tif_file.tiff", "npy_file.npy"]
+_FILENAMES = _VALID_FILENAMES + ["invalid.tif"]
+_USER_CONFIG_ERROR_METHOD = (
+    "pydidas.widgets.misc.select_data_frame_widget."
+    "SelectDataFrameWidget.raise_UserConfigError"
+)
+_VALID_KEYS = sorted(
+    [
+        "/entry/data/2d/data1",
+        "/entry2/dataset/2d/data2",
+        "/entry/data/3d_dataset_1",
+        "/entry2/dataset/3d_dataset_2",
+    ]
+)
+
+
+@pytest.fixture(scope="module")
+def test_data():
+    _3d_data_1 = np.arange(4).reshape(4, 1, 1) * np.ones((1, 5, 6), dtype=int)
+    _3d_data_2 = np.arange(6).reshape(1, 6, 1) * np.ones((5, 1, 7), dtype=int)
+    return {
+        "/entry/data/2d/data1": np.arange(30).reshape(5, 6),
+        "/entry2/dataset/2d/data2": np.arange(42).reshape(6, 7),
+        "/entry/data/3d_dataset_1": _3d_data_1,
+        "/entry2/dataset/3d_dataset_2": _3d_data_2,
+        "/entry2/data/4d_dataset": np.arange(120).reshape(2, 3, 4, 5),
+        "/entry/data/5d_data": np.arange(240).reshape(2, 3, 4, 5, 2),
+    }
+
+
+@pytest.fixture(scope="module", autouse=True)
+def _cleanup():
+    yield
+    app = PydidasQApplication.instance()
+    for widget in [
+        _w for _w in app.topLevelWidgets() if isinstance(_w, SelectDataFrameWidget)
+    ]:
+        widget.deleteLater()
+    app.processEvents()
+
+
+@pytest.fixture(scope="module")
+def path_w_data_files(temp_path, test_data):
+    _path = temp_path / "select_data_frame_widget_files"
+    _path.mkdir()
+    with h5py.File(_path / "hdf5_file.h5", "w") as f:
+        for _dataset_path, _data in test_data.items():
+            f[_dataset_path] = _data
+    with h5py.File(_path / "empty_hdf5_file.h5", "w") as f:
+        pass
+    np.save(_path / "npy_file.npy", test_data["/entry/data/2d/data1"])
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", category=UserWarning)
+        imsave(
+            _path / "tif_file.tiff", test_data["/entry/data/2d/data1"].astype(np.uint16)
+        )
+    yield _path
+
+
+@pytest.fixture
+def widget(qtbot):
+    widget = SelectDataFrameWidget()
+    widget.spy_sig_file_valid = SignalSpy(widget.sig_file_valid)
+    widget.spy_sig_new_selection = SignalSpy(widget.sig_new_selection)
+    qtbot.add_widget(widget)
+    widget.show()
+    qtbot.wait_until(lambda: widget.isVisible(), timeout=500)
+    return widget
+
+
+def assert_hdf5_widget_visibility(widget, should_be_visible: bool):
+    assert widget.isVisible()
+    assert widget.param_composite_widgets["filename"].isVisible()
+    assert (
+        widget.param_composite_widgets["hdf5_key_str"].isVisible() == should_be_visible
+    )
+    _slicing_vis = should_be_visible and widget._selection.axis is not None
+    assert widget.param_composite_widgets["slicing_axis"].isVisible() == _slicing_vis
+    assert widget._widgets["index_selector"].isVisible() == _slicing_vis
+
+
+@pytest.mark.gui
+def test__creation(widget):
+    assert isinstance(widget, SelectDataFrameWidget)
+    assert widget.get_param_value("filename", dtype=str) == "."
+    assert_hdf5_widget_visibility(widget, False)
+
+
+@pytest.mark.gui
+@pytest.mark.parametrize("filename", _FILENAMES)
+@pytest.mark.parametrize("dtype", [str, Path])
+def test_process_new_filename(qtbot, widget, path_w_data_files, filename, dtype):
+    widget.set_param_and_widget_value(
+        "filename", dtype(path_w_data_files / filename), emit_signal=False
+    )
+    if filename == "invalid.tif":
+        with pytest.raises(UserConfigError):
+            widget.process_new_filename()
+        _h5_widgets_should_be_visible = False
+    else:
+        with qtbot.waitSignal(widget.sig_file_valid, timeout=1000):
+            widget.process_new_filename()
+        _h5_widgets_should_be_visible = filename == "hdf5_file.h5"
+    qtbot.wait(5)  # ensure signal processing
+    assert_hdf5_widget_visibility(widget, _h5_widgets_should_be_visible)
+    assert widget.spy_sig_file_valid.n == 1
+    assert widget.spy_sig_file_valid.results[0] == [filename != "invalid.tif"]
+    assert widget.spy_sig_new_selection.n == (filename != "invalid.tif")
+
+
+@pytest.mark.gui
+@pytest.mark.parametrize("filename", _VALID_FILENAMES)
+def test__select_file(qtbot, widget, path_w_data_files, test_data, filename):
+    _fname_widget = widget.param_composite_widgets["filename"]
+    with (
+        qtbot.waitSignal(_fname_widget.sig_new_value, timeout=1000),
+        qtbot.waitSignal(_fname_widget.sig_value_changed, timeout=1000),
+    ):
+        widget.set_param_and_widget_value("filename", path_w_data_files / filename)
+    qtbot.wait(5)  # ensure signal processing
+    assert_hdf5_widget_visibility(widget, filename == "hdf5_file.h5")
+    assert widget.spy_sig_file_valid.n == 1
+    assert widget.spy_sig_file_valid.results[0] == [True]
+    assert widget.spy_sig_new_selection.n == 1
+    assert widget.spy_sig_new_selection.results[0][0] == str(
+        path_w_data_files / filename
+    )
+    if filename == "hdf5_file.h5":
+        for _key, _data in test_data.items():
+            _included = _data.ndim <= 3
+            assert (_key in widget.params["hdf5_key_str"].choices) == _included
+            assert (
+                _key
+                in widget.param_composite_widgets[
+                    "hdf5_key_str"
+                ].io_widget.current_choices
+            ) == _included
+
+
+@pytest.mark.gui
+def test__select_invalid_file(widget, path_w_data_files):
+    widget.set_param_value("filename", path_w_data_files / "invalid.txt")
+    with patch(_USER_CONFIG_ERROR_METHOD) as mock_error:
+        widget.param_composite_widgets["filename"].set_value(
+            widget.get_param_value("filename")
+        )
+    mock_error.assert_called_once()
+    assert_hdf5_widget_visibility(widget, False)
+
+
+@pytest.mark.gui
+def test__select_empty_h5file(qtbot, widget, path_w_data_files):
+    widget.set_param_value("filename", path_w_data_files / "empty_hdf5_file.h5")
+    with pytest.raises(UserConfigError):
+        widget.process_new_filename()
+    assert_hdf5_widget_visibility(widget, False)
+
+
+@pytest.mark.gui
+def test__select_h5file_with_same_dsets(qtbot, widget, path_w_data_files):
+    widget.set_param_and_widget_value_and_choices(
+        "hdf5_key_str", _VALID_KEYS[0], _VALID_KEYS, emit_signal=False
+    )
+    with qtbot.waitSignal(widget.sig_new_selection, timeout=1000):
+        widget.set_param_and_widget_value(
+            "filename", path_w_data_files / "hdf5_file.h5"
+        )
+    assert_hdf5_widget_visibility(widget, True)
+    assert (
+        widget.param_composite_widgets["hdf5_key_str"].display_value == _VALID_KEYS[0]
+    )
+
+
+@pytest.mark.gui
+def test__select_h5_2d_dset_after_3d(widget, path_w_data_files):
+    widget.set_param_and_widget_value("filename", path_w_data_files / "hdf5_file.h5")
+    widget.set_param_and_widget_value("hdf5_key_str", "/entry/data/3d_dataset_1")
+    widget.set_param_and_widget_value("hdf5_key_str", "/entry/data/2d/data1")
+    assert_hdf5_widget_visibility(widget, True)
+    assert (
+        widget.param_composite_widgets["hdf5_key_str"].display_value
+        == "/entry/data/2d/data1"
+    )
+    assert widget._selection.axis is None
+
+
+@pytest.mark.gui
+@pytest.mark.parametrize("dataset", _VALID_KEYS)
+@pytest.mark.parametrize("emulate_gui", [True, False])
+def test__select_hdf5_dset(
+    qtbot, widget, test_data, path_w_data_files, dataset, emulate_gui
+):
+    _data_dim = test_data[dataset].ndim
+    widget.set_param_and_widget_value("filename", path_w_data_files / "hdf5_file.h5")
+    # ensure that the dataset is not currently selected
+    _current_dset = widget.param_composite_widgets["hdf5_key_str"].display_value
+    if _current_dset == dataset:
+        _new_key = _VALID_KEYS[1] if dataset == _VALID_KEYS[0] else _VALID_KEYS[0]
+        widget.set_param_and_widget_value("hdf5_key_str", _new_key, emit_signal=False)
+        _current_dset = _new_key
+    # A single selection signal should have been emitted for the file selection
+    assert widget.spy_sig_file_valid.n == 1
+    assert widget.spy_sig_new_selection.n == 1
+    if emulate_gui:
+        _io_widget = widget.param_composite_widgets["hdf5_key_str"].io_widget
+        _new_index = _io_widget.current_choices.index(dataset)
+        with qtbot.waitSignal(widget.sig_new_selection, timeout=1000):
+            qtbot.mouseClick(_io_widget, QtCore.Qt.LeftButton)
+            _io_widget.setCurrentIndex(_new_index)
+    else:
+        with qtbot.waitSignal(widget.sig_new_selection, timeout=1000):
+            widget.set_param_and_widget_value("hdf5_key_str", dataset)
+    assert widget.spy_sig_new_selection.n == 2
+    assert widget.spy_sig_new_selection.results[1][1]["dataset"] == dataset
+    assert widget.param_composite_widgets["slicing_axis"].isVisible() == (_data_dim > 2)
+    assert widget._widgets["index_selector"].isVisible() == (_data_dim > 2)
+
+
+@pytest.mark.gui
+@pytest.mark.parametrize("axis", [0, 1, 2])
+def test__select_slicing_axis(qtbot, widget, path_w_data_files, axis):
+    widget.set_param_and_widget_value("filename", path_w_data_files / "hdf5_file.h5")
+    widget.set_param_and_widget_value("hdf5_key_str", "/entry/data/3d_dataset_1")
+    _io_widget = widget.param_composite_widgets["slicing_axis"].io_widget
+    _expected_result = (None,) * axis + (
+        widget._widgets["index_selector"].current_slice.start,
+    )
+    if _io_widget.current_text == str(axis):
+        _new_axis = 1 if axis == 0 else 0
+        widget.set_param_and_widget_value("slicing_axis", _new_axis, emit_signal=False)
+    assert _io_widget.current_text != str(axis)
+    # 2 signals should have been emitted for file and dataset selection
+    assert widget.spy_sig_new_selection.n == 2
+    with qtbot.waitSignal(widget.sig_new_selection, timeout=1000):
+        _io_widget.setCurrentIndex(axis)
+    assert widget.spy_sig_new_selection.n == 3
+    assert widget.spy_sig_new_selection.results[2][1]["indices"] == _expected_result
+
+
+@pytest.mark.gui
+@pytest.mark.parametrize("index", [2, 3])
+def test__select_new_frame__valid_index(qtbot, widget, path_w_data_files, index):
+    widget.set_param_and_widget_value("filename", path_w_data_files / "hdf5_file.h5")
+    widget.set_param_and_widget_value("hdf5_key_str", "/entry/data/3d_dataset_1")
+    widget.set_param_and_widget_value("slicing_axis", 1)
+    assert widget.spy_sig_new_selection.n == 3
+    _index_selector = widget._widgets["index_selector"]
+    with qtbot.waitSignal(widget.sig_new_selection, timeout=1000):
+        _index_selector._move_to_index(index)
+    assert widget.spy_sig_new_selection.n == 4
+    _expected_result = (None, index)
+    assert widget.spy_sig_new_selection.results[3][1]["indices"] == _expected_result
+
+
+@pytest.mark.gui
+def test__select_new_frame__same_index(qtbot, widget, path_w_data_files):
+    widget.set_param_and_widget_value("filename", path_w_data_files / "hdf5_file.h5")
+    widget.set_param_and_widget_value("hdf5_key_str", "/entry/data/3d_dataset_1")
+    widget.set_param_and_widget_value("slicing_axis", 1)
+    assert widget.spy_sig_new_selection.n == 3
+    _index_selector = widget._widgets["index_selector"]
+    _index_selector._move_to_index(0)
+    qtbot.wait(20)  # ensure signal processing
+    assert widget.spy_sig_new_selection.n == 3
+
+
+@pytest.mark.gui
+def test__select_new_frame__change_axis(qtbot, widget, path_w_data_files, test_data):
+    _dset = "/entry/data/3d_dataset_1"
+    _spy = widget.spy_sig_new_selection
+    widget.set_param_and_widget_value("filename", path_w_data_files / "hdf5_file.h5")
+    widget.set_param_and_widget_value("hdf5_key_str", _dset)
+    widget.set_param_and_widget_value("slicing_axis", 2)
+    _data = test_data[_dset]
+    assert _spy.n == 3
+    _index_selector = widget._widgets["index_selector"]
+    with qtbot.waitSignal(widget.sig_new_selection, timeout=1000):
+        _index_selector._move_to_index(_data.shape[2] - 1)
+    assert _spy.n == 4
+    _expected_result = (None, None, _data.shape[2] - 1)
+    assert _spy.results[3][1]["indices"] == _expected_result
+    # change to an axis with less points and check that the index resets to 0
+    with (
+        qtbot.waitSignal(widget.sig_new_selection, timeout=1000),
+        warnings.catch_warnings(),
+    ):
+        warnings.simplefilter("ignore", category=UserWarning)
+        widget.set_param_and_widget_value("slicing_axis", 1)
+    assert _spy.n == 5
+    assert _spy.results[4][1]["indices"] == (None, 0)
+    # now move to the last index of axis 1
+    with qtbot.waitSignal(widget.sig_new_selection, timeout=1000):
+        _index_selector._move_to_index(_data.shape[1] - 1)
+    assert _spy.n == 6
+    assert _spy.results[5][1]["indices"] == (None, _data.shape[1] - 1)
+    # and change to axis 2, which should keep the index
+    with qtbot.waitSignal(widget.sig_new_selection, timeout=1000):
+        widget.set_param_and_widget_value("slicing_axis", 2)
+    assert _spy.n == 7
+    assert _spy.results[6][1]["indices"] == (None, None, _data.shape[1] - 1)
+
+
+if __name__ == "__main__":
+    pytest.main([])

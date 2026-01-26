e31358198a2d5432422fb5d7e3ecffa98e76ceae
Malte Storm <malte.storm@hereon.de>
2026-01-26 10:11:06 +0100

    Updated version

--- pydidas/version.py
+++ pydidas/version.py
@@ -23,7 +23,7 @@ Module with the pydidas Version number.
 __author__ = "Malte Storm"
 __copyright__ = "Copyright 2024 - 2025, Helmholtz-Zentrum Hereon"
 __license__ = "GPL-3.0-only"
-__version__ = "25.10.28"
+__version__ = "26.01.26"
 __maintainer__ = "Malte Storm"
 __status__ = "Production"
 

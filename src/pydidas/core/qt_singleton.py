212b97749a9648a1dc87c6a42d4b457caab3bc60
Malte Storm <malte.storm@hereon.de>
2026-06-09 11:14:04 +0200

    Updated QtSingleton and cleaned up methods

--- src/pydidas/core/qt_singleton.py
+++ src/pydidas/core/qt_singleton.py
@@ -27,13 +27,101 @@ __status__ = "Production"
 __all__ = ["QtSingleton"]
 
 
-import copy
-from typing import Any, ClassVar
+import copy as copy_module
+from typing import Any, Callable, ClassVar
 
 from qtpy.QtCore import QObject  # type: ignore[import-not-found]
 
 from pydidas.core.object_with_parameter_collection import ObjectWithParameterCollection
 from pydidas.core.parameter_collection import ParameterCollection
+
+
+def _get_base_class(obj: Any) -> type[ObjectWithParameterCollection]:
+    """
+    Get the base class for a singleton object.
+
+    Parameters
+    ----------
+    obj : Any
+        The singleton object instance.
+
+    Returns
+    -------
+    type[ObjectWithParameterCollection]
+        The base class for the singleton.
+    """
+    # Imported here to avoid circular reference before class definition
+    return QtSingleton._base_classes[obj.__class__]  # type: ignore[attr-defined]
+
+
+def _make_copy(self: Any, copy_function: Callable) -> object:
+    """
+    Make a copy of the base class with the specified copy function.
+
+    This is used when the base class doesn't have a custom __copy__ or
+    __deepcopy__ method. It creates a new base class instance and copies
+    the params and config attributes.
+
+    Parameters
+    ----------
+    self : Any
+        The singleton object instance.
+    copy_function : Callable
+        The copy function to use (copy.copy or copy.deepcopy).
+
+    Returns
+    -------
+    object
+        The copied object.
+    """
+    _base_class = _get_base_class(self)
+    _copy_instance = _base_class()
+    _param_copy: ParameterCollection = copy_function(  # type: ignore[type]
+        getattr(self, "params", ParameterCollection())
+    )
+    if hasattr(_copy_instance, "add_params"):
+        _copy_instance.add_params(_param_copy)
+    _config_copy: dict[str, Any] = copy_function(getattr(self, "_config", {}))
+    if _config_copy:
+        _copy_instance._config = _config_copy
+    return _copy_instance
+
+
+def __copy__(self: Any) -> object:
+    """Create a copy as the base class, not the singleton."""
+    _base_class = _get_base_class(self)
+    if hasattr(_base_class, "__copy__"):
+        # Use temp instance to ensure custom __copy__ doesn't trigger singleton
+        _temp = _base_class.__new__(_base_class)
+        _temp.__dict__.update(self.__dict__)
+        return _base_class.__copy__(_temp)
+    return _make_copy(self, copy_module.copy)
+
+
+def __deepcopy__(self: Any, _memo: dict = {}) -> object:
+    """Create a deep copy as the base class, not the singleton."""
+    _base_class = _get_base_class(self)
+    if hasattr(_base_class, "__deepcopy__"):
+        # Use temp instance to ensure custom __deepcopy__ doesn't trigger singleton
+        _temp = _base_class.__new__(_base_class)
+        _temp.__dict__.update(self.__dict__)
+        return _base_class.__deepcopy__(_temp, _memo)
+    return _make_copy(self, copy_module.deepcopy)
+
+
+def reset_instance(kls: type) -> None:
+    """
+    Reset the singleton instance.
+
+    This allows a fresh instance to be created on the next instantiation.
+    Useful for testing or reinitializing singleton state.
+
+    Parameters
+    ----------
+    kls : type
+        The singleton class to reset.
+    """
+    QtSingleton.reset_instance(kls)
 
 
 class QtSingleton(type(QObject)):
@@ -50,6 +138,7 @@ class QtSingleton(type(QObject)):
     """
 
     _instances: ClassVar[dict[type, type[QObject] | None]] = {}
+    _base_classes: ClassVar[dict[type, type[QObject]]] = {}
 
     def __new__(cls, name: str, bases: tuple[type], dct: dict) -> "QtSingleton":
         """
@@ -69,46 +158,28 @@ class QtSingleton(type(QObject)):
         QtSingleton
             The newly created singleton metaclass instance.
         """
-        # Store original __copy__ and __deepcopy__ if they exist
-        original_copy = dct.get("__copy__")
-        original_deepcopy = dct.get("__deepcopy__")
-
-        # Get the first base class which is not instantiated through the
-        # QtSingleton:
-        _base_class = ObjectWithParameterCollection
+        # Get the first base class which is not a QtSingleton:
+        _base_class: type[ObjectWithParameterCollection] = ObjectWithParameterCollection
         if bases:
             _non_context_bases = list(
                 _b for _b in bases[0].__mro__ if type(_b) is not cls
             )
             if _non_context_bases:
-                _base_class = _non_context_bases[0]
+                _base_class = _non_context_bases[0]  # type: ignore[assignment]
 
-        if not issubclass(_base_class, ObjectWithParameterCollection):
-            raise TypeError(
-                "The QtSingleton class must inherit from "
-                "ObjectWithParameterCollection or derived classes."
-            )
+        # Add required methods to class dictionary
+        dct["__copy__"] = __copy__
+        dct["__deepcopy__"] = __deepcopy__
+        dct["_make_copy"] = _make_copy
+        if hasattr(_base_class, "copy"):
+            dct["copy"] = __copy__
+        if hasattr(_base_class, "deepcopy"):
+            dct["deepcopy"] = __deepcopy__
+        dct["reset_instance"] = classmethod(reset_instance)
 
-        def __copy__(self):
-            """Create a copy as the base class, not the singleton."""
-            _copy_instance = _base_class()
-            _copy_instance.params = copy.copy(  # type: ignore[type]
-                getattr(self, "params", ParameterCollection())
-            )
-            _copy_instance._config = copy.copy(self._config)
-            return _copy_instance
-
-        def __deepcopy__(self, _memo):
-            """Create a deep copy as the base class, not the singleton."""
-            _copy_instance = _base_class()
-            _copy_instance.params = self.params.copy()
-            _copy_instance._config = copy.deepcopy(self._config)
-            return _copy_instance
-
-        dct["__copy__"] = original_copy or __copy__
-        dct["__deepcopy__"] = original_deepcopy or __deepcopy__
-
-        return super().__new__(cls, name, bases, dct)
+        _singleton_class = super().__new__(cls, name, bases, dct)
+        cls._base_classes[_singleton_class] = _base_class
+        return _singleton_class
 
     def __call__(cls, *args: Any, **kwargs: Any) -> Any:  # type: ignore[reportSelfClsParameterName]
         """
@@ -124,3 +195,38 @@ class QtSingleton(type(QObject)):
         if cls not in cls._instances:
             cls._instances[cls] = super().__call__(*args, **kwargs)
         return cls._instances[cls]
+
+    @classmethod
+    def get_base_class(cls, kls: type) -> type[object]:
+        """
+        Get the base class of the singleton class.
+
+        Parameters
+        ----------
+        kls : type
+            The singleton class to get the base class from.
+
+        Returns
+        -------
+        type[object]
+            The base class of the singleton class.
+        """
+        _base_class: type[object] | None = cls._base_classes.get(kls, None)
+        if _base_class is None:
+            raise KeyError(
+                f"The class type {kls} is not stored in the singleton instance."
+            )
+        return _base_class
+
+    @classmethod
+    def reset_instance(cls, kls: type[QObject]) -> None:
+        """
+        Reset the singleton instance.
+
+        Parameters
+        ----------
+        kls : type[QObject]
+            The singleton class to reset.
+        """
+        if kls in QtSingleton._instances:  # type: ignore[attr-defined]
+            del QtSingleton._instances[kls]  # type: ignore[attr-defined]
--- tests/core/test_qt_singleton.py
+++ tests/core/test_qt_singleton.py
@@ -43,10 +43,34 @@ class _NonContextClass(ObjectWithParameterCollection):
         )
 
 
+class _NonContextClassWithCustomCopy(ObjectWithParameterCollection):
+    """Testing class to test QtSingleton."""
+
+    def __init__(self):
+        ObjectWithParameterCollection.__init__(self)
+        self.add_params(
+            Parameter("test1", int, 1),
+            Parameter("test2", int, 2),
+        )
+
+    def __copy__(self) -> "_NonContextClassWithCustomCopy":
+        # Use self.__class__ for elegant subclass support;
+        # the QtSingleton metaclass ensures this is the actual base class
+        # (not a singleton), so this creates the correct type
+        _copy = self.__class__()
+        _copy.is_copy = True
+        return _copy
+
+
 class _NonContextSubClass(_NonContextClass): ...
 
 
 class _ContextClass(_NonContextClass, metaclass=QtSingleton): ...
+
+
+class _ContextClassWithCustomCopy(
+    _NonContextClassWithCustomCopy, metaclass=QtSingleton
+): ...
 
 
 class _SubContextClass(_ContextClass): ...
@@ -102,19 +126,82 @@ def test_init__w_multiple_contexts():
     assert QtSingleton._instances[_SubContextClass] == objC
 
 
-def test_init__w_incompatible_non_context_class():
-    with pytest.raises(TypeError):
+def test_init__w_class_without_params():
+    class OtherClass(PydidasQsettingsMixin, metaclass=QtSingleton): ...
 
-        class IncompatibleClass(PydidasQsettingsMixin, metaclass=QtSingleton): ...
+    assert getattr(OtherClass, "params", None) is None
+    assert getattr(OtherClass, "_config", None) is None
 
 
 @pytest.mark.parametrize("singleton_class", _CONTEXT_CLASSES)
-def test_copy(singleton_class):
+def test_reset_instance__basic(singleton_class):
+    """Test that reset_instance removes the only the stored singleton instance."""
+    _ = [_cls() for _cls in _CONTEXT_CLASSES]
+    assert singleton_class in QtSingleton._instances
+    singleton_class.reset_instance()
+    for _cls in _CONTEXT_CLASSES:
+        if _cls is singleton_class:
+            assert _cls not in QtSingleton._instances
+        else:
+            assert _cls in QtSingleton._instances
+
+
+@pytest.mark.parametrize("singleton_class", _CONTEXT_CLASSES)
+def test_reset_instance__creates_new_instance(singleton_class):
+    """Test that after reset, a new instance is created on next call."""
+    obj1 = singleton_class()
+    obj1_id = id(obj1)
+    singleton_class.reset_instance()
+    obj2 = singleton_class()
+    obj2_id = id(obj2)
+    assert obj1_id != obj2_id
+    assert obj1 is not obj2
+
+
+@pytest.mark.parametrize("singleton_class", _CONTEXT_CLASSES)
+def test_reset_instance__multiple_resets(singleton_class):
+    """Test that reset_instance can be called multiple times safely."""
+    obj1 = singleton_class()
+    singleton_class.reset_instance()
+    obj2 = singleton_class()
+    singleton_class.reset_instance()
+    obj3 = singleton_class()
+    singleton_class.reset_instance()
+    assert id(obj1) != id(obj2)
+    assert id(obj2) != id(obj3)
+    assert id(obj1) != id(obj3)
+
+
+def test_reset_instance__preserves_state_until_call():
+    """Test that resetting instance doesn't affect existing references."""
+    obj1 = _ContextClass()
+    obj1.set_param_value("test1", 42)
+    original_value = obj1.get_param_value("test1")
+    _ContextClass.reset_instance()
+    obj2 = _ContextClass()
+    assert obj1.get_param_value("test1") == original_value
+    assert obj2.get_param_value("test1") == 1  # default value
+
+
+@pytest.mark.parametrize("singleton_class", _CONTEXT_CLASSES)
+def test_reset_instance__on_empty_stack(singleton_class):
+    """Test that reset_instance silently succeeds even if no instance exists."""
+    assert singleton_class not in QtSingleton._instances
+    singleton_class.reset_instance()
+    assert singleton_class not in QtSingleton._instances
+
+
+@pytest.mark.parametrize("singleton_class", _CONTEXT_CLASSES)
+@pytest.mark.parametrize("copy_module", [True, False])
+def test_copy(singleton_class, copy_module):
     obj = singleton_class()
     obj._config["test_key"] = 42
     obj._config["is_false"] = False
     obj.add_param(Parameter("test3", int, 21))
-    obj_copy = obj.copy()
+    if copy_module:
+        obj_copy = copy.copy(obj)
+    else:
+        obj_copy = obj.copy()
     assert id(obj) != id(obj_copy)
     assert isinstance(obj_copy, _NonContextClass)
     for _context_class in _CONTEXT_CLASSES:
@@ -123,6 +210,22 @@ def test_copy(singleton_class):
     assert obj_copy.get_param("test3") is not obj.get_param("test3")
     for _key, _val in obj._config.items():
         assert obj_copy._config[_key] == _val
+
+
+@pytest.mark.parametrize("copy_module", [True, False])
+def test_copy__w_custom_copy(copy_module):
+    obj = _ContextClassWithCustomCopy()
+    obj._config["test_key"] = 42
+    obj._config["is_false"] = False
+    if copy_module:
+        obj_copy = copy.copy(obj)
+    else:
+        obj_copy = obj.copy()
+    assert id(obj) != id(obj_copy)
+    assert isinstance(obj_copy, _NonContextClassWithCustomCopy)
+    assert getattr(obj_copy, "is_copy", False)
+    assert obj_copy._config == {}
+    assert not isinstance(obj_copy, _ContextClassWithCustomCopy)
 
 
 @pytest.mark.parametrize("singleton_class", _CONTEXT_CLASSES)
